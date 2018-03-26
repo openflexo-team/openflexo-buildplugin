7d86ce0889d29686f2e7b3cc2f5c4baa2eb47354
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2018-03-26 11:18:26 -0400

    LOW / update version of pamela and gina

--- buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
+++ buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
@@ -14,9 +14,9 @@ class OpenFlexoBuildConfig implements Plugin<Project> {
 
         project.openflexo.utilsVersion = "1.5-SNAPSHOT";
         project.openflexo.connieVersion = "1.5-SNAPSHOT";
-        project.openflexo.pamelaVersion = "1.4.1-SNAPSHOT";
+        project.openflexo.pamelaVersion = "1.5-SNAPSHOT";
 
-        project.openflexo.ginaVersion = "2.1.1-SNAPSHOT"
+        project.openflexo.ginaVersion = "2.2-SNAPSHOT"
         project.openflexo.dianaVersion = "1.5-SNAPSHOT"
 
         project.openflexo.openflexoVersion = "2.0.0-SNAPSHOT"
