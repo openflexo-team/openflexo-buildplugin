4625c65fd77c11e283e3968a66f75431f9b96323
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2019-01-09 16:49:11 +0100

    MEDIUM / distinct repository for java 10 builds

--- buildplugin/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
+++ buildplugin/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
@@ -422,7 +422,11 @@ class OpenFlexoBuild implements Plugin<Project> {
                 contextUrl = 'https://maven.openflexo.org/artifactory'
                 publish {
                     repository {
-                        repoKey =(project.version.endsWith('-SNAPSHOT')) ? 'openflexo-snapshot' : 'openflexo-release' // The Artifactory repository key to publish to
+                    	def repo = (project.version.endsWith('-SNAPSHOT')) ? 'openflexo-snapshot' : 'openflexo-release'
+                     	if (project.hasProperty('java10')) {
+                     	  repo = 'openflexo-java10'     
+                     	}
+	                	repoKey = repo// The Artifactory repository key to publish to
                         username = "$System.env.ARTIFACTORY_USER" // The publisher user name
                         password = "$System.env.ARTIFACTORY_PASSWORD" // The publisher password
                     }
