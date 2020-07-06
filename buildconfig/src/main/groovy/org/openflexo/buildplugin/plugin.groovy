d31b88de86b74ebcc197f313001fb3d77b0b2643
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2020-07-06 12:59:58 +0200

    IMPORTANT / Use Pamela 1.6 in 2.1.0 version of infrastructure

--- buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
+++ buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
@@ -31,7 +31,7 @@ class OpenFlexoBuildConfig implements Plugin<Project> {
 
         project.openflexo.utilsVersion = "1.5.1" + extension
         project.openflexo.connieVersion = "1.5.1" + extension
-        project.openflexo.pamelaVersion = "1.5.1" + extension
+        project.openflexo.pamelaVersion = "1.6" + extension
 
         project.openflexo.ginaVersion = "2.2.1" + extension
         project.openflexo.dianaVersion = "1.5.1" + extension
