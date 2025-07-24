e4d39c78b53e6dbefce766f14eb3ac8a05e6c94e
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2025-07-24 15:49:38 +0200

    MEDIUM / Fixed component versions for 3.0 version of infrastructure

--- buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
+++ buildconfig/src/main/groovy/org/openflexo/buildplugin/plugin.groovy
@@ -29,17 +29,17 @@ class OpenFlexoBuildConfig implements Plugin<Project> {
 
         // Beware connie version is used by the plugin to add the dependency to testutils
         // In a future version, we could put all versions in ext and use them in the plugin
-        project.ext.connieVersion = "2.0.0" + extension
+        project.ext.connieVersion = "2.1.0" + extension
         project.apply plugin: 'org.openflexo.buildplugin'
 
-        project.openflexo.utilsVersion = "1.6" + extension
+        project.openflexo.utilsVersion = "1.7" + extension
         project.openflexo.connieVersion = project.ext.connieVersion
-        project.openflexo.pamelaVersion = "1.6" + extension
+        project.openflexo.pamelaVersion = "1.7" + extension
 
-        project.openflexo.ginaVersion = "2.3" + extension
-        project.openflexo.dianaVersion = "1.6" + extension
+        project.openflexo.ginaVersion = "2.4" + extension
+        project.openflexo.dianaVersion = "1.7" + extension
 
-        project.openflexo.openflexoVersion = "2.99" + extension
+        project.openflexo.openflexoVersion = "3.0.0" + extension
     }
 }
 
